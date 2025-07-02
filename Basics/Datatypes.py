7fac3ac3680dd0a88dc69a4572aa6f1ee234c360
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2025-07-02 22:14:10 +0530

    data types added

---
+++ Basics/Datatypes.py
@@ -0,0 +1,7 @@
+# The primitive data types are int,float,str,bool
+
+a=4             #int type
+b=3.14          #float type
+c="Ibrahim"     #this is string
+d=True          #boolean that have only true and false
+e=None          #it is Nothing
