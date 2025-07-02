d5628ea9a80865fd9c5c232a072bdf08a758c930
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2025-07-02 22:42:50 +0530

    Conditional statements added

---
+++ Conditional Statements/Ifelse.py
@@ -0,0 +1,5 @@
+age = 16
+if age >= 18:
+    print("You can vote!")
+else:
+    print("You're too young to vote.")
---
+++ Conditional Statements/ifelifelse.py
@@ -0,0 +1,9 @@
+marks = 85
+if marks >= 90:
+    print("A grade")
+elif marks >= 75:
+    print("B grade")
+elif marks >= 60:
+    print("C grade")
+else:
+    print("Fail")
