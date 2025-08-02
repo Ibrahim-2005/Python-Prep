0a84307bbdd3daa8373bc9b0fe87f8c474c92a8b
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2025-08-02 17:29:07 +0530

    Local scope done

--- 06.Functions_py/Local_scope.py
+++ 06.Functions_py/Local_scope.py
@@ -3,7 +3,7 @@ x=6     #it is a global scope
 def local_scope():
     x=9
     print(x)
-
-local_scope()
+local_scope()       # This will print the x inside of function
+print(x)            # This will print the x outside of function
 
 # Local variable is a variable that can be accessed only in the function in which it is declared
