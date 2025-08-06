a3ca6fbfa7d41aa2e8acc60ea10b4f02aa6e08bd
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2025-08-06 20:32:25 +0530

    Modifications added

--- DSA/Lists/Lists_modification.py
+++ DSA/Lists/Lists_modification.py
@@ -5,3 +5,8 @@ print(list1)
 list1[-1]="Dragon Fruit"            # changes the last element as "Dragon Fruit"
 print(list1)
 
+list2=["Onion","Tomato","Potato"]
+list3=list1+list2
+print(list3)
+del list3
+# print(list3)      It throws error cause it is not present now
