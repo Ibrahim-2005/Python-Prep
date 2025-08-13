7ee7e325eac8ae473e39ec9b288d745a9eaf26df
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2025-08-13 22:44:46 +0530

    String slicing done

--- DSA/Topics/String/String_slicing.py
+++ DSA/Topics/String/String_slicing.py
@@ -1,3 +1,17 @@
-proverb="An apple a day keeps a doctor away"
+#String accessing
+name="Ibrahim"
+print(name[5])
+print(name[0])
+print(name[3])
+print(name[6])
 
+
+#String slicing
+
+proverb="An apple a day keeps a doctor away"
 print(proverb[0:9])     #variable[start_index:end_index]
+
+print(proverb[0:len(proverb):3])     #variable[start_index:end_index:step value]
+
+print(proverb[::-1])                 # gives the output in reverse order
+
